b89f397ae3e4b7e13ffb415d219d7a515fae12b6
chksong <chksong@qq.com>
2015-08-14 16:54:37 +0800

    修饰 UITableVIew的 偏移，分割线的偏移 等外观

--- ZhouYi/ZYViewController.m
+++ ZhouYi/ZYViewController.m
@@ -30,6 +30,12 @@
     
     self.tableView.dataSource = self ;
     self.tableView.delegate = self ;
+    
+    //调整tableview的位置。 向下移动
+    [self.tableView setContentInset:UIEdgeInsetsMake(16, 0 , 16, 0)];
+    
+    //去掉多余的 横线
+    [self.tableView setTableFooterView:[[UIView alloc] init] ];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,6 +52,42 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+// 在窗口显示的时候，navigationController 隐藏
+-(void) viewDidAppear:(BOOL)animated  {
+  //  NSLog(@"%s", __func__) ;
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+}
+
+// 在窗口退出的时候，还原导航栏显示
+-(void) viewDidDisappear:(BOOL)animated  {
+  //  NSLog(@"%s", __func__) ;
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
+}
+
+//去掉分割线的 偏移
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsZero];
+    }
+    
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
+}
+
+//去掉分割线的 偏移
+-(void)viewDidLayoutSubviews {
+    if ([self.tableView respondsToSelector:@selector(setSeparatorInset:)]) {
+        [self.tableView setSeparatorInset:UIEdgeInsetsMake(0,0,0,0)];
+    }
+    if ([self.tableView respondsToSelector:@selector(setLayoutMargins:)]) {
+        [self.tableView setLayoutMargins:UIEdgeInsetsMake(0,0,0,0)];
+    }
+}
+
+
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
